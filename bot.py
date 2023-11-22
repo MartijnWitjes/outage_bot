88776dfc3e1d4cacbbd7cbf542f7f9ad8b682fb9
luka <luka.olegsson@gmail.com>
2023-11-22 14:17:35 +0100

    expand config functionality, clarify config errors, more honest output with regards to duration uncertainty

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import asyncio
 import json
+import os
 
 from datetime import datetime
+from dataclasses import dataclass
 from telegram import Bot
 from ping3 import ping
 
@@ -15,18 +17,42 @@ and look for "chat":{"id":-zzzzzzzzzz,
 Put this in a config.json file that follows the structure of the config_example.json file in this repository.
 '''
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
-    bot_token = config['telegram_bot_token']
-    group_chat_id = config['telegram_group_chat_id']
+@dataclass
+class Config:
+    telegram_bot_token: str
+    telegram_group_chat_id: str
+    asdf: str
+    ping_timeout_seconds: int = 20
+    ping_ip: str = '8.8.8.8'
+
+class ConfigNotFound(Exception):
+    pass
+
+class ConfigIncomplete(Exception):
+    pass
+
+try:
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+        config = Config(**config)
+except FileNotFoundError:
+    raise ConfigNotFound(
+        f"config.json missing from {os.getcwd()}, " \
+        "please follow the instructions in README.md"
+    )
+except TypeError:
+    raise ConfigIncomplete(
+        f"config.json in {os.getcwd()} is missing some settings, " \
+        "please follow the instructions in README.md"
+    )
 
 # Initialize Telegram Bot
-bot = Bot(token=bot_token)
+bot = Bot(token=config.telegram_bot_token)
 
 # How do we test whether the internet works? By pinging!
 def is_internet_on():
     try:
-        response = ping('8.8.8.8')
+        response = ping(config.ping_ip)
         return response is not None
     except Exception:
         return False
@@ -41,8 +67,9 @@ async def main():
                 # Internet just came back
                 outage_end = datetime.now()
                 outage_duration = (outage_end - outage_start).total_seconds() / 60  # duration in minutes
-                message = f"Internet outage ended. Duration: {outage_duration:.2f} minutes"
-                await bot.send_message(chat_id=group_chat_id, text=message)
+                message = f"Internet outage ended. Duration: {outage_duration:.2f} " \
+                    f"+- {config.ping_timeout_seconds:.2f} minutes"
+                await bot.send_message(chat_id=config.telegram_group_chat_id, text=message)
                 connected = True
         else:
             if connected:
@@ -50,7 +77,7 @@ async def main():
                 outage_start = datetime.now()
                 connected = False
 
-        await asyncio.sleep(20)  # Check every 20 seconds
+        await asyncio.sleep(config.ping_timeout_seconds)  # Check every 20 seconds
 
 if __name__ == "__main__":
     asyncio.run(main())
